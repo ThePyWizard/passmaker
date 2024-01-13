9080a400269446286e3b6af799c34a496cbddeb9
Jishnu M M <jishnumm4312@gmail.com>
2024-01-13 11:14:46 +0530

    base working model done

--- popup.js
+++ popup.js
@@ -1,20 +1,35 @@
 document.addEventListener('DOMContentLoaded', function() {
     var generateButton = document.getElementById('generatePassword');
-    var passwordContainer = document.getElementById('passwordContainer');
-  
+    var passwordOutput = document.getElementById('password-output');
+    var copyButton = document.getElementById('copyPassword');
+
     generateButton.addEventListener('click', function() {
-      var password = generateRandomPassword();
-      passwordContainer.innerText = password;
+        var password = generateRandomPassword();
+        console.log(password);
+        passwordOutput.innerText = password;
     });
-  });
-  
-  function generateRandomPassword() {
+
+    copyButton.addEventListener('click', function() {
+        var password = passwordOutput.innerText;
+        copyToClipboard(password);
+    });
+});
+
+function generateRandomPassword() {
     var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+";
     var password = "";
     for (var i = 0; i < 12; i++) {
-      var randomIndex = Math.floor(Math.random() * charset.length);
-      password += charset.charAt(randomIndex);
+        var randomIndex = Math.floor(Math.random() * charset.length);
+        password += charset.charAt(randomIndex);
     }
     return password;
-  }
-  
+}
+
+function copyToClipboard(text) {
+    var textarea = document.createElement('textarea');
+    textarea.value = text;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    document.body.removeChild(textarea);
+}
